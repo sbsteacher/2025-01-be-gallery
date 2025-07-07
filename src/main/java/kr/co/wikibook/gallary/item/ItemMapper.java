921907efafaf9ce6e5b9e80eddccbc7fe4704f7b
sbsteacher <id@gmail.com>
2025-07-07 16:12:08 +0900

    first

---
+++ src/main/java/kr/co/wikibook/gallary/item/ItemMapper.java
@@ -0,0 +1,7 @@
+package kr.co.wikibook.gallary.item;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ItemMapper {
+}
---
+++ src/test/java/kr/co/wikibook/gallary/GallaryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wikibook.gallary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
